1b65d46474aaa9106a0f03ef614afb747a14c456
BhargavPabbaraju <bhargav.n.shyam@gmail.com>
2025-03-02 16:07:53 -0500

    Click each cell only once

--- src/index.js
+++ src/index.js
@@ -9,19 +9,11 @@ const state = {
   turn: 0,
 };
 
-renderInitialContent(state.player.gameboard);
-
-function getRowColFromId(id) {
-  const row = id.split("-")[1];
-  const col = id.split("-")[2];
-  return [row, col];
-}
-
 const controller = {
-  clickCell(id) {
-    console.log(id);
-    const cell = document.getElementById(id);
-    const [row, col] = getRowColFromId(id);
+  clickCell(event) {
+    const cell = event.target;
+    const [_, row, col] = cell.id.split("-").map(Number);
+    cell.removeEventListener("click", controller.clickCell);
     state.computer.gameboard.at(row, col)
       ? cell.appendChild(renderIcon(icons.SHIP))
       : cell.appendChild(renderIcon(icons.WATER));
@@ -29,3 +21,4 @@ const controller = {
 };
 
 window.controller = controller;
+renderInitialContent(state.player.gameboard);
--- src/view/index.js
+++ src/view/index.js
@@ -1,26 +1,25 @@
 import "./styles.css";
 import { icons, renderIcon } from "./icons";
 
-function renderCell(id, content = "") {
+function renderCell(id, row = -1, col = -1, content = "") {
   const cell = document.createElement("span");
   cell.setAttribute("id", id);
   cell.classList.add("grid-cell");
   cell.innerText = content;
-  if (id?.startsWith("computer")) {
-    cell.addEventListener("click", () => {
-      window.controller.clickCell(id);
-    });
+  if (id?.startsWith("computer") && row > -1 && col > -1) {
+    cell.addEventListener("click", window.controller.clickCell);
   }
   return cell;
 }
 
 function renderGrid(id, gridSize) {
   const div = document.createElement("div");
+  div.classList.add("blue-border");
   const numberRow = document.createElement("div");
   numberRow.appendChild(renderCell());
   numberRow.classList.add("row");
   for (let i = 0; i < gridSize; i++) {
-    numberRow.appendChild(renderCell(`${id}-row-${i}`, i));
+    numberRow.appendChild(renderCell(`${id}-row-${i}`, -1, -1, i));
   }
   div.appendChild(numberRow);
 
@@ -28,9 +27,9 @@ function renderGrid(id, gridSize) {
     const row = document.createElement("div");
     row.classList.add("row");
     div.appendChild(row);
-    row.appendChild(renderCell(`${id}-col-${i}`, i));
+    row.appendChild(renderCell(`${id}-col-${i}`, -1, -1, i));
     for (let j = 0; j < gridSize; j++) {
-      row.appendChild(renderCell(`${id}-${i}-${j}`));
+      row.appendChild(renderCell(`${id}-${i}-${j}`, i, j));
     }
   }
 
@@ -43,7 +42,6 @@ function renderPlayerShips(playerBoard) {
   for (let i = 0; i < playerBoard.size; i++) {
     for (let j = 0; j < playerBoard.size; j++) {
       if (playerBoard.at(i, j) !== null) {
-        console.log(i, j);
         const cell = document.getElementById(`player-${i}-${j}`);
         cell.appendChild(renderIcon(icons.SHIP));
       }
